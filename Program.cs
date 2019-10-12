71fff2c7647f303f44fbd4e5bbb4508451e41803
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 09:38:59 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ifandifelse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* bool agree = true;
+            if (agree)
+            {
+                Console.WriteLine("Agree");
+                Console.WriteLine("With agree");
+            }
+            else
+            {
+                Console.WriteLine("Dis Agree");
+                Console.WriteLine("With dis agree");
+            } */
+            int guess = 11;
+            if(guess == 3){
+                Console.WriteLine("three");
+            }else if (guess == 1){
+                Console.WriteLine("One");
+            }else if (guess == 2){
+                Console.WriteLine("Two");
+            }else{
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
